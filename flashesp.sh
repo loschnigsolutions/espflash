b6511b235efa8974bacd094c1ab7f2b867f5fa2d
G Moses Loschnig <loschnigsolutions@gmail.com>
2018-09-14 14:01:45 +0530

    ESP8266 Flash Tool

--- flashesp.sh
+++ flashesp.sh
@@ -4,5 +4,5 @@ git clone https://github.com/espressif/esptool.git
 
 sudo apt-get install python-serial
 
-./esptool/esptool.py -p  /dev/ttyUSB1  write_flash 0x00000 ./ai-thinker-0.9.5.2-115200.bin 
+./esptool/esptool.py -p  /dev/ttyUSB0  write_flash 0x00000 ./ai-thinker-0.9.5.2-115200.bin 
 
